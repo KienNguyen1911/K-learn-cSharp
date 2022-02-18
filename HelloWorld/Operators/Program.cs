f013378d735f644861fdf6ea068599a669d20a1f
KLuftmensch <kienndbhaf200045@fpt.edu.vn>
2022-02-18 11:57:08 +0700

    Learn C#

---
+++ HelloWorld/Operators/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any number x");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter any number y");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("x + y = " +(x + y));
+            Console.WriteLine("x + y = "+(x - y));
+            Console.WriteLine("x + y = " +(x * y));
+            Console.WriteLine("x + y = " + (x / y));
+            Console.WriteLine("x % y = " + (x % y));
+            Console.WriteLine("x++ =" + (x++));
+            Console.WriteLine("x-- =" + (x--));
+
+            Console.WriteLine("-----------------------------");
+            // ??? : &=, |=, ^=, >>=, <<=
+
+            int z = 10;
+            Console.WriteLine(z >= 9 && z < 11); // True
+            Console.WriteLine(z > 9 || z < 8);  //True
+            Console.WriteLine(!(z >= 9 && z < 11)); // False
+             
+
+        }
+    }
+}
--- HelloWorld/UserInput/Program.cs
+++ HelloWorld/UserInput/Program.cs
@@ -6,7 +6,16 @@ namespace UserInput
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.ReadLine() return method STRING so you can not get information from another data types
+            Console.WriteLine("Enter your name:");
+            string yourName = Console.ReadLine();
+
+            Console.WriteLine("Enter your age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Your name is " + yourName);
+            Console.WriteLine("Your age is " + age);
+
         }
     }
 }
